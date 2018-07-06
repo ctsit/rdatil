e26cf3549b1b1f199e23cd95b1b9431a93d991c2
Marly Cormar <marlycormar@gmail.com>
2018-07-06 17:04:17 -0400

    Change base dir and remove dir creation.

--- R/read_warrior_files_functions.R
+++ R/read_warrior_files_functions.R
@@ -7,18 +7,13 @@ get_latest_warrior_data <- function(from_directory='~/Downloads/') {
   # Returns:
   #   a list containing two files: the latest warrior data and the latest dictionary data
   # from the given from_directory
-  #
 
-  subDir <- "warrior_data"
-  download_directory <- file.path(from_directory, subDir)
-  dir.create(file.path(from_directory, subDir), showWarnings = FALSE)
-
-  # locate latest files for each input
+  # Locates latest files for each input
   input <- c("data", "dictionary")
   pattern <- c("WARRIOR_DATA_20\\d\\d.*\\.csv", "WARRIOR_DataDictionary_20\\d\\d.*\\.csv")
   input_files <- data.frame(input, pattern, stringsAsFactors=FALSE)
   latest_file <- function(x, dir) head(sort(list.files(path = dir, pattern = x), decreasing=TRUE), n=1)
-  latest <- unlist(lapply(input_files$pattern , FUN=latest_file, dir=download_directory))
+  latest <- unlist(lapply(input_files$pattern , FUN=latest_file, dir=from_directory))
   input_files <- data.frame(input_files, latest, stringsAsFactors=FALSE)
 
   return(input_files)
