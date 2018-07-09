4ac7ffbf338f6879d28f30c77b1cf6fc7ab770a7
Marly Cormar <marlycormar@gmail.com>
2018-07-09 16:05:10 -0400

    Improve description on warrior_specific_functions.R.

--- R/warrior_specific_functions.R
+++ R/warrior_specific_functions.R
@@ -1,15 +1,16 @@
 get_latest_warrior_data_file_names <- function(from_directory='~/Downloads/') {
-  # Gets the latest (in terms of date) downloaded warrior data and metadata data file
-  # names from the provided from_directory. If no arguments are provided, the
-  # directory is assumed to be the '~/Downloads/' folder.
+  # Gets the latest downloaded warrior data and metadata data files
+  # names (in terms of date) from the provided from_directory. If no
+  # arguments are provided, the directory is assumed to be the '~/Downloads/' folder.
   #
   # Args:
   #   from_directory a directory where to find the latest data
   # Returns:
-  #   a list containing two files: the latest warrior data and the latest dictionary data
-  # from the given from_directory
+  #   a list containing two strings: the latest warrior data and the latest
+  #   dictionary data file names. To access them, you may use
+  #   returned_list[['data']] to get the data file name and
+  #   returned_list[['metadata']] to get the metadata file name.
 
-  # Locates latest files for each input
   input <- c("data", "metadata")
   pattern <- c("WARRIOR_DATA_20\\d\\d.*\\.csv", "WARRIOR_DataDictionary_20\\d\\d.*\\.csv")
   find_latest_data <- function(x, dir) head(sort(list.files(path = dir, pattern = x), decreasing=TRUE), n=1)
