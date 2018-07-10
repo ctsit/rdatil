fb539ca968d78d8500b8f37f3a529526069d03af
Marly Cormar <marlycormar@gmail.com>
2018-07-10 12:46:49 -0400

    Remove returns between pipes as it breaks the script.

--- R/get_categorical_field_labels.R
+++ R/get_categorical_field_labels.R
@@ -9,13 +9,10 @@ get_categorical_field_labels <- function(metadata) {
   # Returns:
   #   a dataframe with the columns, "field_name", "form_name", "key", and "value"
   #
-  categorical_fields <- metadata %>%
-    dplyr::filter(field_type %in% c("checkbox", "radio", "yesno")) %>%
-    dplyr::select(c(field_name, field_label, form_name, select_choices_or_calculations))
+  categorical_fields <- metadata %>% dplyr::filter(field_type %in% c("checkbox", "radio", "yesno")) %>% dplyr::select(c(field_name, field_label, form_name, select_choices_or_calculations))
 
   # Split columns into rows by '|'
-  categorical_fields <- categorical_fields %>% dplyr::mutate(select_choices_or_calculations = strsplit(as.character(select_choices_or_calculations), "\\|\\s?"))
-  %>% unnest(select_choices_or_calculations)
+  categorical_fields <- categorical_fields %>% dplyr::mutate(select_choices_or_calculations = strsplit(as.character(select_choices_or_calculations), "\\|\\s?")) %>% unnest(select_choices_or_calculations)
 
   # Split columns into two columns by ','
   categorical_fields <- categorical_fields %>% separate(select_choices_or_calculations, c("key", "value"), sep="\\,", extra = "merge")
