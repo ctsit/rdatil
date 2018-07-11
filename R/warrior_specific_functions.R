985205ddea15d2b0073cc3b4b64df59dd73530aa
Marly Cormar <marlycormar@gmail.com>
2018-07-11 16:39:32 -0400

    Make function get_latest_data_file_names generic.

--- R/warrior_specific_functions.R
+++ R/warrior_specific_functions.R
@@ -1,25 +1,24 @@
-get_latest_warrior_data_file_names <- function(from_directory='~/Downloads/') {
-  # Gets the latest downloaded warrior data and metadata data filenames
-  #(in terms of date) from the provided from_directory, as well as the date
-  # reflected in the filenames. If no arguments are provided, the directory
-  # is assumed to be the '~/Downloads/' folder.
+get_latest_data_file_names <- function(from_directory='~/Downloads/', patterns) {
+  # Gets the latest downloaded files (in terms of date) from the provided
+  # from_directory, as well as the date reflected in the filenames.
+  # If no arguments are provided, the directory is assumed to be the '~/Downloads/' folder.
   #
   # Args:
-  #   a directory where to find the latest data.
+  #   from_directory = a directory where to find the latest data,
+  #   patterns = key-value pairs containing label and regex patter.
+  # Example:
+  #   patterns <- c("data" = "WARRIOR_DATA_20\\d\\d.*\\.csv", "date" = "\\d{4}-\\d{2}-\\d{2}")
   # Returns:
-  #   a list containing of three key-value paris: the latest warrior data filename, the latest
-  #   dictionary data filename, and the date data was downloaded.
-  #   To access them, you may use
+  #   a list containing of key-value pairs where the keys are the labels in 'patterns'
+  #   and the values are the latest file names according to the patterns.
+  # Examples:
   #   returned_list[['data']] to get the data file name and
-  #   returned_list[['metadata']] to get the metadata file name
   #   returned_list[['date']] to get the date associated with the data.
-  input <- c("data", "metadata", "date")
-  pattern <- c("WARRIOR_DATA_20\\d\\d.*\\.csv", "WARRIOR_DataDictionary_20\\d\\d.*\\.csv")
   find_latest_data <- function(x, dir) head(sort(list.files(path = dir, pattern = x), decreasing=TRUE), n=1)
-  file_names <- unlist(lapply(pattern , FUN=find_latest_data, dir=from_directory))
-  date <- stringr::str_extract(file_names[1], "\\d{4}-\\d{2}-\\d{2}")
+  file_names <- unlist(lapply(patterns[names(patterns) != 'date'] , FUN=find_latest_data, dir=from_directory))
+  date <- stringr::str_extract(file_names[1], patterns['date'])
   file_names <- c(file_names, date)
-  names(file_names) <- input
+  names(file_names) <- c(names(patterns[names(patterns) != 'date']), 'date')
 
   return(file_names)
 }
