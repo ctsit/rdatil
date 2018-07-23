cdc15cdc045cca144d06e9af2618701f067f6210
Marly Cormar <marlycormar@gmail.com>
2018-07-23 17:01:57 -0400

    Include comments on the script trim_repeating_instruments that follows roxygen2 format.

--- R/trim_repeating_instruments.R
+++ R/trim_repeating_instruments.R
@@ -1,10 +1,10 @@
+#' Removes repeating instruments.
+#'
+#' @param records A data frame containing the records.
+#' @return A data frame containing the records with no repeating elements.
+#' @examples
+#' trim_repeating_instruments(records)
 trim_repeating_instruments <- function(records) {
-  # Removes repeating instruments.
-  #
-  # Args:
-  #   the records
-  # Returns:
-  #   the records with no repeating instruments
   records <- records %>% filter(redcap_repeat_instrument == "" | is.na(redcap_repeat_instrument)) %>% select(-redcap_repeat_instrument, -redcap_repeat_instance)
 
   return(records)
