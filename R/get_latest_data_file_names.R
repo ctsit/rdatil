192d886140bdc43335ee4a2727f8c0c6a17dd673
Marly Cormar <marlycormar@gmail.com>
2018-07-23 17:00:54 -0400

    Include comments on the script get_latest_data_file_names that follows roxygen2 format.

--- R/get_latest_data_file_names.R
+++ R/get_latest_data_file_names.R
@@ -1,19 +1,15 @@
+#' Gets the latest downloaded files (in terms of date) from the provided
+#' from_directory, as well as the date reflected in the filenames.
+#' If no arguments are provided, the directory is assumed to be the '~/Downloads/' folder.
+#'
+#' @param from_directory A directory where to find the latest data.
+#' @param patterns A key-value pairs vector containing the label and regex pattern.
+#' @return A vector containing key-value pairs where the keys are the labels in 'patterns'
+#'         and the values are the latest file names according to the patterns.
+#' @examples
+#' get_latest_data_file_names(from_directory, patterns)
+#' get_latest_data_file_names(patterns = patterns)
 get_latest_data_file_names <- function(from_directory='~/Downloads/', patterns) {
-  # Gets the latest downloaded files (in terms of date) from the provided
-  # from_directory, as well as the date reflected in the filenames.
-  # If no arguments are provided, the directory is assumed to be the '~/Downloads/' folder.
-  #
-  # Args:
-  #   from_directory = a directory where to find the latest data,
-  #   patterns = key-value pairs containing label and regex patter.
-  # Example:
-  #   patterns <- c("data" = "WARRIOR_DATA_20\\d\\d.*\\.csv", "date" = "\\d{4}-\\d{2}-\\d{2}")
-  # Returns:
-  #   a list containing of key-value pairs where the keys are the labels in 'patterns'
-  #   and the values are the latest file names according to the patterns.
-  # Examples:
-  #   returned_list[['data']] to get the data file name and
-  #   returned_list[['date']] to get the date associated with the data.
   find_latest_data <- function(x, dir) head(sort(list.files(path = dir, pattern = x), decreasing=TRUE), n=1)
   file_names <- unlist(lapply(patterns[names(patterns) != 'date'] , FUN=find_latest_data, dir=from_directory))
   date <- stringr::str_extract(file_names[1], patterns['date'])
