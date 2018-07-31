28c20ae6723dbda0806fa04f2109bec0c421a330
Marly Cormar <marlycormar@gmail.com>
2018-07-31 14:54:16 -0400

    Fix example data for trim_repeating_instruments.R.

--- R/trim_repeating_instruments.R
+++ R/trim_repeating_instruments.R
@@ -4,7 +4,7 @@
 #' @return A data frame containing the records with no repeating elements.
 #' @export
 #' @examples
-#' trim_repeating_instruments(records)
+#' trim_repeating_instruments(Medication_records)
 trim_repeating_instruments <- function(records) {
   records <- records %>% filter(redcap_repeat_instrument == "" | is.na(redcap_repeat_instrument)) %>% select(-redcap_repeat_instrument, -redcap_repeat_instance)
 
