2259f0c111e57f3b107886e2817c932e1501eb06
Marly Cormar <marlycormar@gmail.com>
2018-07-31 13:25:38 -0400

    Update roxygen comments and fix tidyr missing package import.

--- R/get_categorical_field_labels.R
+++ R/get_categorical_field_labels.R
@@ -4,12 +4,10 @@
 #' @param metadata A REDCap metadata file with columns in the standardized format defined in
 #'        https://github.com/SpectrumHealthResearch/REDCapRITS/blob/master/R/R/REDCap_split.r
 #' @return A dataframe with the columns, "field_name", "field_type", form_name", "code", and "label".
-#' @import dplyr
-#' @importFrom utils head
+#' @import dplyr tidyr
 #' @export
 #' @examples
-#' metadata <- read.csv('AnimalIdentification_DataDictionary.csv')
-#' get_categorical_field_labels(metadata)
+#' get_categorical_field_labels(AnimalIdentification_DataDictionary)
 get_categorical_field_labels <- function(metadata) {
   categorical_fields <- metadata %>%
     dplyr::filter(field_type %in% c("checkbox", "radio", "yesno", "dropdown")) %>%
@@ -22,7 +20,7 @@ get_categorical_field_labels <- function(metadata) {
   # Split columns into rows by '|'
   categorical_fields <- categorical_fields %>%
     dplyr::mutate(select_choices_or_calculations = strsplit(as.character(select_choices_or_calculations), "\\|\\s?")) %>%
-    unnest(select_choices_or_calculations)
+    tidyr::unnest(select_choices_or_calculations)
 
   # Split columns into two columns by ','
   categorical_fields <- categorical_fields %>%
