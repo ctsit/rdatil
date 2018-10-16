a7be2b1821244204736892086bf74d748665630e
Marly Cormar <marlycormar@ufl.edu>
2018-10-16 09:48:37 -0400

    Fix innacurate count.

--- R/print_table.R
+++ R/print_table.R
@@ -11,12 +11,12 @@
 print_table <- function(data, main_column_name) {
   fig <- as.data.frame(round(prop.table(table(data))*100, digits = 1))
   fig <- fig %>%
-    mutate (Count = as.data.frame(table(data))$Freq) %>%
+    left_join(., as.data.frame(table(data)), by="data") %>%
     mutate (!!main_column_name := data) %>%
     mutate (
-      `Percent (Count)` = paste0(Freq, "% (", Count, ")")
+      `Percent (Count)` = paste0(Freq.x, "% (", Freq.y, ")")
     ) %>%
-    select(-Freq, -Count, -data)
+    select(-Freq.x, -Freq.y, -data)
 
   return(knitr::kable(fig, booktabs = T) %>% kableExtra::kable_styling(latex_options = "striped"))
 }
